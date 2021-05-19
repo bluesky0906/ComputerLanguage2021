a09b79e4516ae16bc6fcb7aecf26189b34edc16b
Sora Tagami <loslonbervene.1923@icloud.com>
2021-05-19 22:01:08 +0900

    question 3-1, 3-2

--- src/Class03.hs
+++ src/Class03.hs
@@ -13,16 +13,29 @@ module Class03 where
   class DataF (f :: * -> *) where
     fmap :: (a -> b) -> f a -> f b
     occur :: f Bool -> Bool
+    draw :: f String -> String
 
   -- GADTs拡張を用いて、data宣言に型注釈を与えている
   data T where
-    Fix :: (DataF t) => t T -> T 
+    Fix :: (DataF f) => f T -> T 
     Meta :: Int -> T
 
+  -- 抽象的にした型T全般に適用できる関数
   occur' :: Int -> T -> Bool
   occur' k fix = case fix of
     Fix d -> occur (Class03.fmap (occur' k) d)
     Meta n -> n == k
+
+  draw' :: T -> String
+  draw' fix = case fix of
+    Fix d -> draw (Class03.fmap draw' d)
+    Meta n -> "{" ++ show n ++ "}"
+
+  -- fold' :: DataF f => (f T -> T) -> T -> T -> T
+  -- fold' :: DataF f => (f a -> b) -> b -> T -> b
+  -- fold' g k fix = case fix of
+  --   Fix d -> g (Class03.fmap (fold' g k) d)
+  --   Meta n -> k
 
   -- Tree module
   data TreeF a = Empty | Node (a, String, Int, a)
@@ -35,6 +48,11 @@ module Class03 where
       Empty -> False
       Node (left, s, n, right) -> left || right
 
+    draw tree = case tree of
+      Empty -> "[]"
+      Node (left, s, n, right) -> "[" ++ left ++ ", " ++ s ++ ", " ++ show n ++ ", " ++ right ++ "]"
+
+    
   -- smart constructor
   empty :: T
   empty = Fix Empty
@@ -50,3 +68,36 @@ module Class03 where
   tree2 = node empty "a" 3 (meta 0)
   tree3 = node tree2 "b" 4 (node empty "c" 1 empty)
   tree4 = node tree2 "b" 0 (node (meta 1) "c" 0 empty)
+
+  -- 1
+  data TypeF a = Int | Arrow (a, a) | Prod (a, a) deriving (Show)
+  instance DataF TypeF where
+    fmap g typ = case typ of
+      Arrow (l, r) -> Arrow (g l, g r)
+      Prod (l, r) -> Prod (g l, g r)
+      Int -> Int
+
+    occur typ = case typ of
+      Int -> False
+      Arrow (l, r) -> l || r
+      Prod (l, r) -> l || r
+
+    draw typ = case typ of
+      Int -> "Int"
+      Arrow (l, r) -> l ++ " -> " ++ r
+      Prod (l, r) -> "(" ++ l ++ ", " ++ r ++ ")"
+
+  -- smart constructor
+  int :: T
+  int = Fix Int
+  arrow :: T -> T -> T
+  arrow l r = Fix (Arrow (l, r))
+  prod :: T -> T -> T
+  prod l r = Fix (Prod (l, r))
+
+  -- 例
+  type1 = int
+  type2 = arrow int (meta 0)
+  type3 = prod int int
+  type4 = arrow (prod (arrow int int) int) type3
+  type5 = arrow (prod type2 int) (arrow type3 (meta 3))
--- test/Class03Spec.hs
+++ test/Class03Spec.hs
@@ -6,16 +6,57 @@ import Class03
 
 spec :: Spec
 spec = do
-  describe "sample in class" $ do
+  describe "occur function for trees" $ do
     it "0 did not occur in tree1" $
       occur' 0 tree1 `shouldBe` False
-    it "0 did not occur in tree2" $
+    it "0 occur in tree2" $
       occur' 0 tree2 `shouldBe` True
     it "1 did not occur in tree3" $
       occur' 1 tree2 `shouldBe` False
-    it "0 did not occur in tree4" $
+    it "0 occur in tree4" $
       occur' 0 tree4 `shouldBe` True
-    it "1 did not occur in tree4" $
+    it "1 occur in tree4" $
       occur' 1 tree4 `shouldBe` True
     it "2 did not occur in tree4" $
-      occur' 2 tree4 == False
+      occur' 2 tree4 `shouldBe` False
+
+  -- 1
+  describe "occur function for types" $ do
+    it "0 did not occur in type1" $
+      occur' 0 type1 `shouldBe` False
+    it "0 did not occur in type2" $
+      occur' 0 type2 `shouldBe` True
+    it "1 did not occur in type3" $
+      occur' 1 type2 `shouldBe` False
+    it "1 did not occur in type4" $
+      occur' 1 type4 `shouldBe` False
+    it "0 occur in type5" $
+      occur' 0 type5 `shouldBe` True
+    it "1 did not occur in type5" $
+      occur' 1 type5 `shouldBe` False
+    it "3 occur in type5" $
+      occur' 3 type5 `shouldBe` True
+
+  -- 2
+  describe "draw function for trees" $ do
+    it "print tree1" $
+      draw' tree1 `shouldBe` "[]"
+    it "print tree2" $
+      draw' tree2 `shouldBe` "[[], a, 3, {0}]"
+    it "print tree3" $
+      draw' tree3 `shouldBe` "[[[], a, 3, {0}], b, 4, [[], c, 1, []]]"
+    it "print tree4" $
+      draw' tree4 `shouldBe` "[[[], a, 3, {0}], b, 0, [{1}, c, 0, []]]"
+  
+  describe "draw function for types" $ do
+    it "print types1" $
+      draw' type1 `shouldBe` "Int"
+    it "print types2" $
+      draw' type2 `shouldBe` "Int -> {0}"
+    it "print types3" $
+      draw' type3 `shouldBe` "(Int, Int)"
+    it "print types4" $
+      draw' type4 `shouldBe` "(Int -> Int, Int) -> (Int, Int)"
+    it "print types5" $
+      draw' type5 `shouldBe` "(Int -> {0}, Int) -> (Int, Int) -> {3}"
+    
