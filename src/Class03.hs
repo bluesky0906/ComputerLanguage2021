e0534b72a5378e94894b101c910f8f955f68b9a6
Sora Tagami <loslonbervene.1923@icloud.com>
2021-05-26 22:45:37 +0900

    update fold

--- src/Class03.hs
+++ src/Class03.hs
@@ -1,6 +1,8 @@
 -- {-# LANGUAGE DatatypeContexts #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
 {-
 ocamlのmoduleのような機能はないため、
 型クラスを使って、DataFが持つべき関数を指定
@@ -12,46 +14,53 @@ module Class03 where
   -- KindSignaturesで種注釈をつけている
   class DataF (f :: * -> *) where
     fmap :: (a -> b) -> f a -> f b
-    occur :: f Bool -> Bool
-    draw :: f String -> String
 
   -- GADTs拡張を用いて、data宣言に型注釈を与えている
   data T where
     Fix :: (DataF f) => f T -> T 
     Meta :: Int -> T
 
-  -- 抽象的にした型T全般に適用できる関数
-  occur' :: Int -> T -> Bool
-  occur' k fix = case fix of
-    Fix d -> occur (Class03.fmap (occur' k) d)
-    Meta n -> n == k
+  -- DataFのインスタンスである型を持つ全てのデータに適用できる
+  -- https://qiita.com/hyone/items/a1a300325253e3c5b7d1
+  fold :: (forall f. DataF f =>  f a -> a) -> (Int -> a) -> T -> a
+  fold phi psi fix = case fix of
+    Fix d -> phi (Class03.fmap (fold phi psi) d)
+    Meta n -> psi n
 
-  draw' :: T -> String
-  draw' fix = case fix of
-    Fix d -> draw (Class03.fmap draw' d)
-    Meta n -> "{" ++ show n ++ "}"
+  occur :: (forall f . DataF f => f Bool -> Bool) -> Int -> T -> Bool
+  occur phi k = fold phi psi
+    -- Meta の場合
+    where psi n = k == n
 
-  -- fold' :: DataF f => (f a -> a) -> (Int -> a) -> T -> a
-  -- fold' phi psi fix = case fix of
-  --   Fix d -> phi (Class03.fmap (fold' phi psi) d)
-  --   Meta n -> psi n
+  draw :: (forall f .　DataF f => f String -> String) -> T -> [Char]
+  draw phi = fold phi psi
+    where psi n = "{" ++ show n ++ "}"
 
-  -- Tree module
+
+  --------- Tree module ----------
   data TreeF a = Empty | Node (a, String, Int, a)
   instance DataF TreeF where
     fmap g tree = case tree of
       Empty -> Empty
       Node (left, s, n, right) -> Node (g left, s, n, g right)
 
-    occur tree = case tree of
-      Empty -> False
-      Node (left, s, n, right) -> left || right
+  -- 木のための関数
+  -- occurTree :: Int -> T -> Bool
+  occurTree = occur phi
+    where
+      -- 1段分
+      phi :: TreeF Bool -> Bool
+      phi tree = case tree of
+        Empty -> False
+        Node (left, s, n, right) -> left || right
 
-    draw tree = case tree of
-      Empty -> "[]"
-      Node (left, s, n, right) -> "[" ++ left ++ ", " ++ s ++ ", " ++ show n ++ ", " ++ right ++ "]"
+  drawTree k tree = draw phi
+    where
+      phi tree = case tree of
+        Empty -> "[]"
+        Node (left, s, n, right) -> "[" ++ left ++ ", " ++ s ++ ", " ++ show n ++ ", " ++ right ++ "]"
+      
 
-    
   -- smart constructor
   empty :: T
   empty = Fix Empty
@@ -68,7 +77,8 @@ module Class03 where
   tree3 = node tree2 "b" 4 (node empty "c" 1 empty)
   tree4 = node tree2 "b" 0 (node (meta 1) "c" 0 empty)
 
-  -- 1
+
+  -- 問1
   data TypeF a = Int | Arrow (a, a) | Prod (a, a) deriving (Show)
   instance DataF TypeF where
     fmap g typ = case typ of
@@ -76,15 +86,19 @@ module Class03 where
       Prod (l, r) -> Prod (g l, g r)
       Int -> Int
 
-    occur typ = case typ of
-      Int -> False
-      Arrow (l, r) -> l || r
-      Prod (l, r) -> l || r
+  occurType typ = occur phi
+    where
+      phi = case typ of
+        Int -> False
+        Arrow (l, r) -> l || r
+        Prod (l, r) -> l || r
 
-    draw typ = case typ of
-      Int -> "Int"
-      Arrow (l, r) -> l ++ " -> " ++ r
-      Prod (l, r) -> "(" ++ l ++ ", " ++ r ++ ")"
+  drawType typ = draw phi
+    where
+      phi =case typ of
+        Int -> "Int"
+        Arrow (l, r) -> l ++ " -> " ++ r
+        Prod (l, r) -> "(" ++ l ++ ", " ++ r ++ ")"
 
   -- smart constructor
   int :: T
