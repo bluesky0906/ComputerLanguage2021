476bc00531b1bdd7957a97f9fc8cbe0cd1a8aaf7
Sora Tagami <loslonbervene.1923@icloud.com>
2021-06-01 23:35:37 +0900

    add Class05

---
+++ src/Class05.hs
@@ -0,0 +1,125 @@
+{-# LANGUAGE GADTs #-}
+
+module Class05 where
+  data CodeT =  U | I | P | Plus (CodeT, CodeT) | Times (CodeT, CodeT) deriving(Show)
+
+  data SumT a b = Left a | Right b deriving(Show)
+
+  data TypeF a p where 
+    Unit :: () -> TypeF a p
+    Rec :: a -> TypeF a p
+    Par :: p -> TypeF a p
+    Sum :: SumT (TypeF a p) (TypeF a p) -> TypeF a p
+    Pair :: (TypeF a p, TypeF a p) -> TypeF a p
+    deriving(Show)
+
+  fmap :: CodeT -> (a -> b) -> TypeF a p -> TypeF b p
+  fmap code f d = case (code, d) of
+    (U,              Unit ())          -> Unit ()
+    (I,              Rec d')         -> Rec (f d')
+    (P,              Par v)          -> Par v
+    (Plus (c1, c2),  Sum (Class05.Left d')) -> Sum (Class05.Left (Class05.fmap c1 f d'))
+    (Plus (c1, c2),  Sum (Class05.Right d')) -> Sum (Class05.Right (Class05.fmap c2 f d'))
+    (Times (c1, c2), Pair (d1, d2))    -> Pair (Class05.fmap c1 f d1, Class05.fmap c2 f d2)
+
+  newtype FixT p = Fix (TypeF (FixT p) p) deriving(Show)
+
+  fold :: CodeT -> (TypeF a p -> a) -> FixT p -> a
+  fold code phi (Fix d) =
+    phi (Class05.fmap code (fold code phi) d)
+
+
+  -- 自然数　-----------------------------------------------------------
+  nat :: CodeT
+  nat = Plus (U, Times (U, I))
+
+  -- constructor-like functions
+  zero :: FixT p
+  zero = Fix (Sum (Class05.Left (Unit ())))
+
+  suc :: FixT p -> FixT p
+  suc n = Fix (Sum (Class05.Right (Pair (Unit (), Rec n))))
+
+  -- 自然数の例
+  one = suc zero
+  two = suc one
+  three = suc two
+
+  length :: FixT p -> Int
+  length = fold nat lengthF
+    where
+      lengthF d = case d of
+        Sum (Class05.Left (Unit ())) -> 0
+        Sum (Class05.Right (Pair (Unit (), Rec n))) -> 1 + n
+
+  -- 自然数のリスト　-----------------------------------------------------------
+  list :: CodeT
+  list = Plus (U, Times (P, I))
+
+  -- constructor-like functions
+  empty :: FixT q
+  empty = Fix (Sum (Class05.Left (Unit ())))
+
+  cons :: q -> FixT q -> FixT q
+  cons n l = Fix (Sum (Class05.Right (Pair (Par n, Rec l))))
+
+  -- 自然数のリストの例
+  lst1 = cons one empty
+  lst2 = cons one (cons two empty)
+  lst3 = cons zero (cons one (cons two (cons three empty)))
+
+  lengthList :: FixT p -> Int
+  lengthList = fold list lengthF
+    where
+      lengthF :: TypeF Int q -> Int  
+      lengthF d = case d of
+        Sum (Class05.Left (Unit ())) -> 0
+        Sum (Class05.Right (Pair (Par n, Rec l))) -> 1 + l
+
+  sumList :: FixT (FixT p) -> Int
+  sumList = fold list sumF
+    where
+      sumF :: TypeF Int (FixT q) -> Int
+      sumF d = case d of
+        Sum (Class05.Left (Unit ())) -> 0
+        Sum (Class05.Right (Pair (Par n, Rec l))) -> Class05.length n + l
+
+  --  　-----------------------------------------------------------
+  tree :: CodeT
+  tree = Plus (U, Times (I, Times (P, I)))
+
+  -- constructor-like functions
+  emptyTree :: FixT p
+  emptyTree = Fix (Sum (Class05.Left (Unit ())))
+
+  node :: FixT p -> p -> FixT p -> FixT p
+  node l n r = Fix (Sum (Class05.Right (Pair (Rec l, Pair (Par n, Rec r)))))
+
+  -- 例
+  -- FixT (FixT nat)型
+  tree1 = emptyTree
+  tree2 = node emptyTree one emptyTree
+  tree3 = node tree2 two (node emptyTree zero tree2)
+  tree4 = node tree3 three tree2
+
+  -- FixT (FixT list)型
+  tree5 = node emptyTree lst1 emptyTree
+  tree6 = node tree5 lst2 (node emptyTree lst1 tree5)
+  tree7 = node tree6 lst3 tree5
+
+  -- 問4
+  countEmpty :: FixT (FixT p) -> Int
+  countEmpty = fold tree countF
+    where
+      countF :: TypeF Int (FixT p) -> Int
+      countF d = case d of
+        Sum (Class05.Left (Unit ())) -> 1
+        Sum (Class05.Right (Pair (Rec l, Pair (Par n, Rec r)))) -> l + r
+
+  -- 問5
+  size :: FixT p -> Int
+  size = fold tree sizeF
+    where
+      sizeF d = case d of
+        Sum (Class05.Left (Unit ())) -> 0
+        Sum (Class05.Right (Pair (Rec l, Pair (Par n, Rec r)))) -> l + r + 1
---
+++ test/Class05Spec.hs
@@ -0,0 +1,72 @@
+module Class05Spec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+import Class05
+
+spec :: Spec
+spec = do
+  -- 4b.mlのテスト
+  describe "nat length test" $ do
+    it "length zero == 0" $
+      Class05.length zero `shouldBe` 0
+    it "length one == 1" $
+      Class05.length one `shouldBe` 1
+    it "length two == 2" $
+      Class05.length two `shouldBe` 2
+    it "length three == 3" $
+      Class05.length three `shouldBe` 3
+
+  -- 4b.mlのテスト
+  describe "lengthList test" $ do
+    it "lengthList empty == 0" $
+      Class05.lengthList empty `shouldBe` 0
+    it "lengthList lst1 == 1" $
+      Class05.lengthList lst1 `shouldBe` 1
+    it "lengthList lst2 == 2" $
+      Class05.lengthList lst2 `shouldBe` 2
+    it "lengthList lst3 == 0" $
+      Class05.lengthList lst3 `shouldBe` 4
+  
+  -- 4b.mlのテスト
+  describe "sumList test" $ do
+    it "sumList empty == 0" $
+      Class05.sumList empty `shouldBe` 0
+    it "lengthList lst1 == 1" $
+      Class05.sumList lst1 `shouldBe` 1
+    it "lengthList lst2 == 2" $
+      Class05.sumList lst2 `shouldBe` 3
+    it "lengthList lst3 == 0" $
+      Class05.sumList lst3 `shouldBe` 6
+
+  describe "Question4. test countEmpty" $ do
+    it "tree1 has 1 empty" $
+      countEmpty tree1 `shouldBe` 1
+    it "tree2 has 2 empty" $
+      countEmpty tree2 `shouldBe` 2
+    it "tree3 has 5 empty" $
+      countEmpty tree3 `shouldBe` 5
+    it "tree4 has 8 empty" $
+      countEmpty tree4 `shouldBe` 7
+    it "tree5 has 2 empty" $
+      countEmpty tree5 `shouldBe` 2
+    it "tree6 has 5 empty" $
+      countEmpty tree6 `shouldBe` 5
+    it "tree7 has 7 empty" $
+      countEmpty tree7 `shouldBe` 7
+
+  describe "Question5. test size" $ do
+    it "tree1 has 0 node" $
+      size tree1 `shouldBe` 0
+    it "tree2 has 1 node" $
+      size tree2 `shouldBe` 1
+    it "tree3 has 4 node" $
+      size tree3 `shouldBe` 4
+    it "tree4 has 5 node" $
+      size tree4 `shouldBe` 6
+    it "tree5 has 1 node" $
+      size tree5 `shouldBe` 1
+    it "tree6 has 4 node" $
+      size tree6 `shouldBe` 4
+    it "tree7 has 5 node" $
+      size tree7 `shouldBe` 6
